008ce87bae75504485c8905087c0120c5aff94c6
Daniel <dpinto@gradarius.com>
2018-09-30 17:11:08 -0400

    Fixedd US36 and added unit tests

--- US36.py
+++ US36.py
@@ -1,4 +1,5 @@
 import datetime
+import unittest
 
 def get_dt_obj(string_date):
     """
@@ -13,8 +14,37 @@ def US36(death):
     if death == '' or death == 'N/A' or death == 'Unknown':
         #Also return true for cases where date is unknown
         return False
-    death = get_dt_obj(death)
+    death_dt = get_dt_obj(death)
     today = datetime.datetime.now()
-    if today - datetime.timedelta(days=30) <= death:
+    if today - datetime.timedelta(days=30) <= death_dt:
         return True
     return False
+
+class TestDeath(unittest.TestCase):
+
+    def test_2_days_ago(self):
+        death = "28-SEPT-2018"
+        self.assertEqual(US36(death), True)
+        return
+
+    def test_over_a_month_ago(self):
+        death = "28-JAN-2018"
+        self.assertEqual(US36(death), False)
+        return
+
+    def test_same_day_and_month_but_different_year(self):
+        death = "28-SEPT-2016"
+        self.assertEqual(US36(death), False)
+        return
+
+    def test_a_day_ago(self):
+        death = "29-SEPT-2018"
+        self.assertEqual(US36(death), True)
+        return
+
+    def test_today(self):
+        death = "30-SEPT-2018"
+        self.assertEqual(US36(death), True)
+        return
+
+
